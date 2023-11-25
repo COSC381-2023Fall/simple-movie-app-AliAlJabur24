86d2edab37950f6b2faa6ace511653bae2a34331
AliAlJabur24 <66652142+AliAlJabur24@users.noreply.github.com>
2023-11-25 17:19:07 -0500

    Add Get

--- backend/main.py
+++ backend/main.py
@@ -5,11 +5,6 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/movies/{movie_id}")
+async def read_movie(movie_id: int):
+    return {"movie_id": movie_id, "movie_data": "Fake movie data for example"}
