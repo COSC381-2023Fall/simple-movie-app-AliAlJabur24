ca23506112c3af7a835520523f4eca6e68f81ddc
AliAlJabur24 <66652142+AliAlJabur24@users.noreply.github.com>
2023-11-28 17:46:14 -0500

    Added Get Method

---
+++ backend/movies.py
@@ -0,0 +1,39 @@
+class Movies:
+    def __init__(self, movies_file):
+        self._movies = []
+
+        with open(movies_file, encoding="utf-8") as file:
+            row_idx = 0
+            for line in file:
+                if row_idx%3 == 0:
+                    movie_name = line.rstrip()
+                if row_idx%3 == 1:
+                    movie_cast = line.rstrip().split(',')
+                if row_idx%3 == 2:
+                    self._movies.append(
+                        {
+                            'movie_id': (len(self._movies) + 1),
+                            'name': movie_name,
+                            'cast': movie_cast
+                        }
+                    )
+                    movie_name = None
+                    movie_cast = None
+                row_idx += 1
+
+        if movie_name and movie_cast:
+            # Add the last movie to the list
+            self._movies.append(
+                {
+                    'movie_id': (len(self._movies) + 1),
+                    'name': movie_name,
+                    'cast': movie_cast
+                }
+            )
+    
+    def get_movie_id(self, movie_id):
+        return self._movies[movie_id - 1]
+        
+
+if __name__ == "__main__":
+    movies = Movies('./movies.txt')
